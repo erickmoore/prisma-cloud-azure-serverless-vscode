c7ea604e3bdecd55eda6c7cfd0483217b759a82a
Erick Moore <erick.moore@gmail.com>
2024-06-16 16:43:44 -0400

    updates

--- src/commands/functions/updateCsprojFile.ts
+++ src/commands/functions/updateCsprojFile.ts
@@ -3,7 +3,9 @@ import { updateConfig, UpdateConfigFile } from '../../utilities/updateConfig';
 export async function updateCsprojFile(csprojFile: string, twistlockVersion: string) {
     const searchString = `<PackageReference Include="Twistlock" Version="${twistlockVersion}" />`;
     const insertAbove = '</Project>';
-    const newContent = `
+    const newContent = `<!-- This function is protected by Prisma Cloud. Do not remove or modify this comment.
+https://docs.prismacloud.io/en/enterprise-edition/content-collections/runtime-security/install/deploy-defender/serverless/serverless -->
+<!-- Start of Prisma Cloud protected section --> -->    
 <ItemGroup>
     <PackageReference Include="Twistlock" Version="${twistlockVersion}" />
     <TwistlockFiles Include="twistlock\\*" Exclude="twistlock\\twistlock.${twistlockVersion}.nupkg" />
@@ -11,9 +13,8 @@ export async function updateCsprojFile(csprojFile: string, twistlockVersion: str
 <ItemGroup>
     <None Include="@(TwistlockFiles)" CopyToOutputDirectory="Always" LinkBase="twistlock\\" />
 </ItemGroup>
+<!-- End of Prisma Cloud protected section --> -->`;
 
-`;
     const successMessage = `Serverless Defender package references added to ${csprojFile}`;
-
     await updateConfig({ file: csprojFile, searchString, insertAbove, newContent, successMessage } as UpdateConfigFile);
 }
--- src/commands/functions/updateNugetConfig.ts
+++ src/commands/functions/updateNugetConfig.ts
@@ -8,13 +8,14 @@ export async function updateNugetConfig() {
     const nugetFile = path.join(workspaceRoot, 'NuGet.Config');
     const searchString = '<add key="local-packages" value="./twistlock/" />';
     const insertAbove = '</configuration>';
-    const newContent = `
+    const newContent = `    <!-- This function is protected by Prisma Cloud. Do not remove or modify this comment.
+    https://docs.prismacloud.io/en/enterprise-edition/content-collections/runtime-security/install/deploy-defender/serverless/serverless -->
+    <!-- Start of Prisma Cloud protected section --> -->
     <packageSources>
     <clear />
         <add key="local-packages" value="./twistlock/" />
         <add key="nuget.org" value="https://api.nuget.org/v3/index.json" protocolVersion="3" />
     </packageSources>
-`;
-
+    <!-- End of Prisma Cloud protected section --> -->`;
     await updateConfig({ file: nugetFile, searchString, insertAbove, newContent } as UpdateConfigFile);
 }
